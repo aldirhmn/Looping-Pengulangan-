0b6b7ab9ede4b466ba234292494afe19c3c47650
Aldi Rahman <36370633+aldirhmn@users.noreply.github.com>
2018-02-13 22:18:19 +0700

    Add files via upload

---
+++ Pengulangan.java
@@ -0,0 +1,25 @@
+
+public class Pengulangan {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++)
+			if (i % 2 == 0) {
+				System.out.println("Aldi : " + i);
+			}else {
+				if (i % 2 == 1) {
+					System.out.println("Rahman : " + i);
+					}else {
+						System.out.println(i);
+				}
+			}
+		
+		for (int i = 0; i <= 100; i++)
+			if (i % 10 == 0) {
+				System.out.println(i + " Aldi Rahman");
+			}else {
+				System.out.println(i);
+			}
+
+	}
+
+}
